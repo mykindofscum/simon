40d178f69cd269a8a428e634086b4b40b24cd58e
mykindofscum <alvinkchan99@gmail.com>
2019-05-08 14:18:56 -0700

    javascript...

--- js/main.js
+++ js/main.js
@@ -16,6 +16,7 @@ const buttonSound = [
     'sound/piano-c.wav',
     'sound/piano-a.wav',
     'sound/piano-g.wav',
+    'sound/correct-sound.wav',
     'sound/Sad_Trombone.mp3'
 ];
 
@@ -24,8 +25,10 @@ const buttonSound = [
 // E note: blue, c# yellow, a red, e octave down: green
 
 /*----- app's state (variables) -----*/
-let game, reset, start, strict;
-const soundArray = [1, 2, 3, 4];
+let sequence, guess, badGuess, reset, gameover, start, strict;
+const soundArray = [1, 2, 3, 4, 5, 6];
+let guessArray = [];
+let seqArray = [];
 
 /*----- cached element references -----*/
 const playEl = document.querySelector("#round h1");
@@ -57,15 +60,15 @@ document.querySelector('#start').addEventListener('click', playStart);
 
 
 
-var soundPlayer = new Audio()
+var soundPlayer = new Audio();
 
 function playStart(evt) {
     console.log(evt.target.getAttribute('data-button'));
-
+    
     debugger;
-
+    
     if (evt.target.id !== "start") {
-    //sound from the buttonSoundArray linked to 'data-button'.
+        //sound from the buttonSoundArray linked to 'data-button'.
         soundPlayer.src = buttonSound[parseInt(evt.target.getAttribute('data-button')) - 1]
         soundPlayer.play()
     } else {
@@ -74,8 +77,10 @@ function playStart(evt) {
     }
     
     //if start button is pressed, run playStart function, game starts
-    this.classList.toggle('active');
     
+
+    this.classList.toggle('active');
+
     var movesArray = [1, 2, 3, 4];
     var offset = 0;
     movesArray.forEach(function(movesArray) {
@@ -83,17 +88,28 @@ function playStart(evt) {
         document.getElementById('button-y').classList.add('active-button');
     setTimeout(function() {
         document.getElementById('button-y').classList.remove('active-button');
-    }, 500);
+    }, 250);
         console.log(Math.floor(Math.random(movesArray) * 4) + 1);
     }, 1000 + offset);
     offset += 1000;
+});
 
-    });
-}
+
+// this.movesArray.push(seqArray);
+    // if (Math.floor(Math.random(movesArray) * 4) + 1 === guessArray) {
+    //     soundPlayer.src = buttonSound[6]
+    //     soundPlayer.play([5]);
+    // }
+
+
+
+
+
+
 
 /*----- functions -----*/
-init();
 
+init();
 function init() {
     // display starts at 1 
     
@@ -114,53 +130,43 @@ function init() {
 
 
 function playStrict() {
+
 }
 
 function showMoves(movesArray) {
     // for each element in round
-        // console.log the numbers in the round, 1 at a time with 1s between logs
-        // var movesArray = [1, 2, 3, 4];
-        // (Math.random(movesArray['']));
-        
-        // var offset = 0;
-        // movesArray.forEach(function(moves) {
+    // console.log the numbers in the round, 1 at a time with 1s between logs
+    // var movesArray = [1, 2, 3, 4];
+    // (Math.random(movesArray['']));
+    
+    // var offset = 0;
+    // movesArray.forEach(function(moves) {
         // setTimeout(function() {
-        //     console.log(moves);
-        // }, 1000 + offset);
-        // offset += 1000;
-        // });
+            //     console.log(moves);
+            // }, 1000 + offset);
+            // offset += 1000;
+            // });
+            
 
-        // TODO: add an "active" class/manipulate style properties to show active button
-
-
-        // TODO: play the associated sound from the 'buttons' const variable
-
-    // calls playStart() / playStrict()
-}
-
-function increaseRound(currentRound) {
-    // add a new element to the end of the currentRound array
-    return Math.floor(Math.random() * 4) + 1;
-    // call render again with the updated sequence
-}
-
-//function getRandomSeq(max) {
-//     return Math.floor(Math.random() * Math.floor(max));
-// }
-// console.log(getRandomSeq(5));
-
-function render(currentRound) {
-    // call showMoves(currentRound)
-
-    showMoves(currentRound);
-    // increment a roundCounter?
-}
-
-
-
-
-
-
-
-// Use Math.floor(Math.random() * length);  to randomize sequence(array of four, one for each button) each turn
-
+            
+            // TODO: play the associated sound from the 'buttons' const variable
+            
+            // calls playStart() / playStrict()
+        }
+        
+        function increaseRound(currentRound) {
+            // add a new element to the end of the currentRound array
+            return Math.floor(Math.random() * 4) + 1;
+            // call render again with the updated sequence
+        }
+        
+        //function getRandomSeq(max) {
+            //     return Math.floor(Math.random() * Math.floor(max));
+            // }
+            // console.log(getRandomSeq(5));
+            
+          
+                // call showMoves(currentRound)
+           
+                // increment a roundCounter?
+    }
