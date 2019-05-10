3c4281e8d9f076713d5c3ebf149c2f35c39a3337
mykindofscum <alvinkchan99@gmail.com>
2019-05-09 19:30:29 -0700

    Final

--- js/main.js
+++ js/main.js
@@ -1,6 +1,5 @@
 
 /*----- constants -----*/ 
-
 const buttonSound = [
     'sound/piano-e.wav',
     'sound/piano-c.wav',
@@ -13,63 +12,40 @@ const winAudio = new Audio('sound/correct-sound.wav');
 const loseAudio = new Audio('sound/8-bit-wrong-2.wav');
 let soundPlayer = new Audio();
 
-// buttonSound.play();
 
 /*----- app's state (variables) -----*/
-let sequence, guess, reset, gameover, start, strict;
+let guess, start;
 let playerArray = [];
-var movesArray = [0, 1, 2, 3];
+let movesArray = [0, 1, 2, 3];
 let buttonId = ['button-y', 'button-b', 'button-r', 'button-g'];
 
+
 /*----- cached element references -----*/
-const playEl = document.querySelector("#round h1");
-const resetEl = document.querySelector("#reset h1");
-const strictEl = document.querySelector("#strict h1");
-
-const buttonPlayY = document.querySelector(".board #button-y");
-parseInt(buttonPlayY.getAttribute('data-button'));
-// > "1"
-const buttonPlayB = document.querySelector(".board #button-b");
-parseInt(buttonPlayB.getAttribute('data-button'));
-
-const buttonPlayG = document.querySelector(".board #button-g");
-parseInt(buttonPlayG.getAttribute('data-button'));
-
-const buttonPlayR = document.querySelector(".board #button-r");
-parseInt(buttonPlayR.getAttribute('data-button'));
-
+const buttonY = document.querySelector(".board #button-y");
+parseInt(buttonY.getAttribute('data-button'));
+const buttonB = document.querySelector(".board #button-b");
+parseInt(buttonB.getAttribute('data-button'));
+const buttonG = document.querySelector(".board #button-g");
+parseInt(buttonG.getAttribute('data-button'));
+const buttonR = document.querySelector(".board #button-r");
+parseInt(buttonR.getAttribute('data-button'));
 const msgEl = document.getElementById('msg');
-
-
 
 /*----- event listeners -----*/
 document.querySelector('.board #button-y').addEventListener('click', playerSays);
 document.querySelector('.board #button-b').addEventListener('click', playerSays);
 document.querySelector('.board #button-g').addEventListener('click', playerSays);
 document.querySelector('.board #button-r').addEventListener('click', playerSays);
-
-// start button init and playStart
 document.querySelector('#start').addEventListener('click', playStart);
-document.querySelector('#reset').addEventListener('click', resetGame);
-
-
-
 
 /*----- functions -----*/
 
-// debugger;
-//if start button is pressed, run playStart function, game starts
-// playStart();
-
-//init();
-
 function playStart(evt) {
-    
+    msgEl.innerHTML = '';
+    playerArray = [];
     simonSays();
 }
 
-
-// Simonsays
 function simonSays() {
     var offset = 0;
     for (let i = 0;i < 4;i++) {
@@ -84,61 +60,33 @@ function simonSays() {
                 document.getElementById(`${buttonId[move]}`).classList.remove('active-button');
             }, 700);
             
-            console.log(move);
         }, 800 + offset);
         
-        offset += 500;
+        offset += 1000;
     });
-}
-
-function resetGame() {
-    playerArray = [];
 }
 
 function playerSays(evt) {
     let soundIndex = evt.target.getAttribute('data-button');
-    
-    // if statement that either plays the right sound
-    // if (soundIndex || winner) {
-    // }
-        // player Wins, prompt replace H1 with You Win
         soundPlayer.src = buttonSound[soundIndex];
         soundPlayer.play();
-        // or plays the wrong sound and calls simonSays()
+        playerArray.push(parseInt(soundIndex));
     
-    // push soundIndex into a playerArray array
-    playerArray.push(parseInt(soundIndex));
-
-    console.log(playerArray);
+        console.log(playerArray);
 
     if (playerArray.length === movesArray.length) {
         getWinner();
-    }
+    } 
 }
 
 function getWinner() {
    
-    
     let result = true;
-
-    // playerArray.forEach(function (e1,i) { 
-    //     movesArray(function (e2) {    
-            // if (e1.length > 1 && e2.length) {
-                // result = compare(e1, e2);
-            // } else 
-            
-    //         if (e1 !== e2 ){
-    //             result = false
-    //         } else {
-    //             result = true
-    //         }
-    //     });
-    // });
+    
     for (let i = 0; i < playerArray.length; i++) {
         if (playerArray[i] !== movesArray[i]) {
             result = false;
         }
-        
     }
     if (result === true) {
         winAudio.play();
@@ -146,27 +94,15 @@ function getWinner() {
         playerArray = [];
     } else {
         loseAudio.play();
-        msgEl.innerHTML = `You Lose!`; 
-        //msg 'You Lost, maybe try again'
-    }
-
+        msgEl.innerHTML = `You Lose!`;
+        playerArray = [];
+       }
 }
 
-    // if (playerArray.length === movesArray.length) {
-    //     winAudio.play();
-    // } else {
-    //     loseAudio.play();
-    // }
-        
-    
-    
-    
-    // document.getElementById("h1").innerHTML = "You Win!";
-        // alert (`You Win!`);
-        // winAudio.play();
-        // playerArray = [];
 
-        // loseAudio.play();
+    
+    
+    
 
 
 
